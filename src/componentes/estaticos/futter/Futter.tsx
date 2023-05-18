f8666ee3e14e2af9b644a6ddd60ded460f1b3119
r4inha <kadu_300live.com>
2023-05-17 21:21:27 -0300

    Atualização Futter

--- src/componentes/estaticos/futter/Futter.tsx
+++ src/componentes/estaticos/futter/Futter.tsx
@@ -8,25 +8,25 @@ function Footer(){
     return(
         <>
            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{backgroundColor: "#DDA0DD", height: "120px"}}>
+                <Grid alignItems="center" item xs={12} >
+                    <Box style={{backgroundColor: "#DDA0DD",height: "120px"}}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{color:"#800F2F"}}>Siga-nos nas redes sociais</Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/UFA-Uniao-Feminina-Agora" target="_blank"><GitHubIcon style={{fontSize: 60, color: "#800F2F"}}/> </a>
-                            <a href="https://www.linkedin.com/in/michellegreghi/" target="_blank"><LinkedInIcon style={{fontSize: 60, color: "#800F2F"}}/> </a>
-                            <a href="michelle.neves23@gmail.com" target="_blank"><MailOutlineIcon style={{fontSize: 60, color: "#800F2F"}}/> </a>
+                            <a href="mailto:michelle.neves23@gmail.com" target="_blank"><MailOutlineIcon style={{fontSize: 60, color: "#800F2F"}}/> </a>
                         </Box>
                     </Box>
-                    <Box style={{backgroundColor:"#DDA0DD", height:"60px"}}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{color:"#C9184A"}}>©️ 2023 Copyright</Typography>
+                    <Box style={{backgroundColor:"#DDA0DD", height:"68px"}}>
+                        <Box paddingTop={0}>
+                            <Typography align="center" gutterBottom style={{color:"#C9184A"}}>©️ 2023 Copyright</Typography>
                         </Box>
-
+                        <Box paddingTop={0}>
+                            <Typography  align="center" gutterBottom style={{color:"#800F2F"}}>Feito com ❤ por Bianca, Carolina, Deiziane, Gabriel, Katarina e Michelle.</Typography>
+                    </Box>
                     </Box>
                 </Grid>
-
            </Grid>
         </>
     )
