709bbe1f788900914cba6edad8d5e3476552eda4
Gabriel Lopes <lopesgabriel810@gmail.com>
2023-05-26 10:50:21 -0300

    LiastTema

---
+++ src/componentes/temas/listatemas/ListaTemas.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import useLocalStorage from 'react-use-localstorage';
+import Tema from '../../../models/Tema'
+import { busca } from '../../../service/Service';
+import { useSelector } from 'react-redux';
+import { UserState } from '../../../store/token/Reducer';
+
+function ListaTema() {
+    const [temas, setTemas] = useState<Tema[]>([])
+    const token = useSelector<UserState, UserState["tokens"]>(
+        (state) => state.tokens
+      );
+    let history = useNavigate();
+
+    useEffect(() => {
+        if (token === '') {
+            alert("Você precisa estar logado")
+            history("/login")
+        }
+
+    }, [token])
+
+
+    async function getTema() {
+        await busca("/temas", setTemas, {
+            headers: {
+
+
+                'Authorization': token
+            }
+
+
+        })
+       
+
+
+
+    } 
+    
+    
+    useEffect(() => { getTema() }, [temas.length])
+    return (
+        <>{
+            temas.map(tema => (
+                <Box m={2}>
+                    <Card variant="outlined">
+                        <CardContent>
+
+                            <Typography color="textSecondary" gutterBottom>
+
+                                tema
+                            </Typography>
+                            <Typography>
+                                {tema.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Box display="flex" justifyContent="center" mb={1.5}>
+
+                                <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                                    <Box mx={1}>
+                                        <Button variant="contained" className="marginLeft" size='small' color='primary'>
+                                            atualizar
+                                        </Button>
+                                    </Box>
+
+                                </Link>
+                                <Link to={`/deletarTema/${tema.id}`} className="text-decorator-nome">
+                                    <Box mx={1}>
+                                        <Button variant='contained' size='small' color="secondary">
+                                            deletar
+                                        </Button>
+                                    </Box>
+
+                                </Link>
+
+                            </Box>
+                        </CardActions>
+
+                    </Card>
+                </Box>))
+
+        }
+
+
+        </>
+
+
+
+
+
+
+    )
+
+
+
+
+
+
+}
+export default ListaTema;
