5432076e5e38430d8b52280950608898782ee434
Michelle Greghi <michelle.neves23@gmail.com>
2023-05-30 14:12:38 -0300

    Atualização

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,126 +1,153 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Box, IconButton, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
-import { Link, useNavigate } from 'react-router-dom';
-import MenuIcon from '@material-ui/icons/Menu';
-import './Navbar.css';
-import useLocalStorage from 'react-use-localstorage';
-import { useDispatch, useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-import { UserState } from '../../../store/token/Reducer';
-import { addToken } from '../../../store/token/Action';
+import React, { useState } from "react";
+import {AppBar, Toolbar, Typography, Box, IconButton, Drawer, List, ListItem, ListItemText} from "@material-ui/core";
+import { Link, useNavigate } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./Navbar.css";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { UserState } from "../../../store/token/Reducer";
+import { addToken } from "../../../store/token/Action";
 
 function Navbar() {
-    // const [token, setToken] = useLocalStorage('token');
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const token = useSelector<UserState, UserState["tokens"]>(
+    (state) => state.tokens
+  );
 
-    const token = useSelector<UserState, UserState["tokens"]>(
-        (state) => state.tokens
-      );
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const toggleDrawer = (open: any) => (event: any) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-    const toggleDrawer = (open: any) => (event: any) => {
-        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
+    setIsDrawerOpen(open);
+  };
 
-        setIsDrawerOpen(open);
-    };
+  function goLogout() {
+    dispatch(addToken(""));
 
-    function goLogout(){
-        dispatch(addToken(""));
-            
-    toast.info('Usuário deslogado!', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable:false,
-        theme:"colored",
-        progress: undefined,
-      });
-  
-      navigate("/login");
-    
-    }    
+    toast.info("Usuário deslogado!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "colored",
+      progress: undefined,
+    });
 
-    return (
-        <>
-            <AppBar position="static"  style={{ backgroundColor: '#ffb3c6' }}>
-                <Toolbar variant="dense">
-                    <img src="src/assets/images/logo_ufa_sem_fundo.png" alt="Logo ufa" width="100px" height="100px" />
-                        <Box marginLeft="auto">
-                            <IconButton
-                                edge="end"
-                                style={{ color: '#ff0054' }}
-                                aria-label="menu"
-                                onClick={toggleDrawer(true)}
-                                
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Drawer
-                                anchor="right"
-                                open={isDrawerOpen}
-                                onClose={toggleDrawer(false)}
-                            >
-                                <List style={{ backgroundColor: '#ffb3c6' }}>
-                                    <Link to="/home" className="text-decorator-none">
-                                        <ListItem button >
-                                            <ListItemText primary="Home" style={{ color: '#ff0054' }}/>
-                                        </ListItem>
-                                    </Link>
-                                    <Link to="/formularioPostagem" className="text-decorator-none">
-                                    <ListItem button>
-                                        <ListItemText primary="Criar Postagem" style={{ color: '#ff0054' }} />
-                                    </ListItem>
-                                    </Link>
-                                    <Link to="/sobre" className="text-decorator-none">
-                                        <ListItem button>
-                                            <ListItemText primary="Sobre" style={{ color: '#ff0054' }} />
-                                        </ListItem>
-                                    </Link>
-                                    <Link to="/postagens" className="text-decorator-none">
-                                    <ListItem button>
-                                        <ListItemText primary="Postagens" style={{ color: '#ff0054' }}/>
-                                    </ListItem>
-                                    </Link>
-                                    <Link to="/temas" className="text-decorator-none">
-                                    <ListItem button>
-                                        <ListItemText primary="Temas" style={{ color: '#ff0054' }} />                                       
-                                    </ListItem>
-                                    </Link>
-                                    <Link to="/formularioTema" className="text-decorator-none">
-                                    <ListItem button>
-                                        <ListItemText primary="Cadastrar Temas" style={{ color: '#ff0054' }} />
-                                    </ListItem>
-                                    </Link>
-                                    <Link to="/contatos" className="text-decorator-none">
-                                        <ListItem button >
-                                            <ListItemText primary="Contatos" style={{ color: '#ff0054' }}/>
-                                        </ListItem>
-                                    </Link>
-                                    <Link to="/perfil" className="text-decorator-none">
-                                        <ListItem button >
-                                            <ListItemText primary="Perfil" style={{ color: '#ff0054' }}/>
-                                        </ListItem>
-                                    </Link>
-                                    {/* <Link to="/login" className="text-decorator-none"> */}
-                                        <ListItem button onClick={goLogout}>
-                                            <ListItemText primary="Logout" style={{color: '#ff0054'}}/>
-                                        </ListItem>
-                                    {/* </Link> */}
-                                </List>
-                            </Drawer>
-                        </Box>
-                </Toolbar>
-            </AppBar>
-        </>
-    );
+    navigate("/login");
+  }
+
+  return (
+    <>
+      <AppBar position="static" style={{ backgroundColor: "#ffb3c6" }}>
+        <Toolbar variant="dense">
+          <Link to="/home" className="text-decorator-none">
+            <img
+              src="src/assets/images/logo_ufa_sem_fundo.png"
+              alt="Logo ufa"
+              width="100px"
+              height="100px"
+            />
+          </Link>
+          <Box marginLeft="auto">
+            <IconButton
+              edge="end"
+              style={{ color: "#ff0054" }}
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={isDrawerOpen}
+              onClose={toggleDrawer(false)}
+            >
+              <List style={{ backgroundColor: "#ffb3c6" }}>
+                <Link to="/home" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText primary="Home" style={{ color: "#ff0054" }} />
+                  </ListItem>
+                </Link>
+                <Link to="/formularioPostagem" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Criar Postagem"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/sobre" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Sobre"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/postagens" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Postagens"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/temas" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Temas"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/formularioTema" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Cadastrar Temas"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/contatos" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Contatos"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                <Link to="/perfil" className="text-decorator-none">
+                  <ListItem button>
+                    <ListItemText
+                      primary="Perfil"
+                      style={{ color: "#ff0054" }}
+                    />
+                  </ListItem>
+                </Link>
+                {/* <Link to="/login" className="text-decorator-none"> */}
+                <ListItem button onClick={goLogout}>
+                  <ListItemText primary="Logout" style={{ color: "#ff0054" }} />
+                </ListItem>
+                {/* </Link> */}
+              </List>
+            </Drawer>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default Navbar;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,80 +1,137 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
-import react, { useEffect } from 'react'
+import { Box, Button, Grid, Typography } from "@mui/material";
+import react, { useEffect } from "react";
 import "./Home.css";
-
-import { Link } from 'react-router-dom';
-
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { UserState } from '../../store/token/Reducer';
-import { toast } from 'react-toastify';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { UserState } from "../../store/token/Reducer";
+import { toast } from "react-toastify";
 
 function Home() {
-    let navigate = useNavigate();
-    const token = useSelector<UserState, UserState["tokens"]>(
-        (state) => state.tokens
-    )
+  let navigate = useNavigate();
+  const token = useSelector<UserState, UserState["tokens"]>(
+    (state) => state.tokens
+  );
 
-    useEffect(() => {
-        if (token === "") {
-            toast.error('Você precisa estar logado!!', {
-                position: "top-right",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable:false,
-                theme:"colored",
-                progress: undefined,
-              });
-            navigate("/login")
+  useEffect(() => {
+    if (token === "") {
+      toast.error("Você precisa estar logado!!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,
+      });
+      navigate("/login");
+    }
+  }, [token]);
 
-        }
-    }, [token])
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "#FF4D6D" }}
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              style={{ color: "white", fontWeight: "bold" }}
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              style={{ color: "white", fontWeight: "bold" }}
+            >
+              Encontre oportunidades de crescimento!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Link to={"/postagens"} className="textDecoration">
+              <Box marginRight={1}></Box>
+              <Button className="botao-postagem" variant="outlined">
+                Ver Postagens
+              </Button>
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="../../../src/assets/images/imagem1.svg"
+            alt="logo"
+            width="550px"
+            height="300px"
+          />
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: "white" }}></Grid>
+      </Grid>
 
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#FF4D6D" }}>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Encontre oportunidades de crescimento!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                    <Link to={'/postagens'} className="textDecoration">  
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button className='botao-postagem' variant="outlined">Ver Postagens</Button>
-                    </Link>    
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="../../../src/assets/images/imagem1.svg" alt="logo" width="550px" height="300px" />
-                </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
-                </Grid>
-            </Grid>
-
-            <Grid container direction="row-reverse" justifyContent="center" alignItems="center" style={{ backgroundColor: "#faf0ca" }}>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ backgroundColor: "#faf0ca", fontWeight: "bold" }}>Ou...</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#ff0054", fontWeight: "bold" }}>Veja os nossos temas!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button className='botao-tema' variant="outlined">Ver Temas</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="../../../src/assets/images/imagem2.svg" alt="logo" width="550px" height="300px" />
-                </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
-                </Grid>
-            </Grid>
-        </>
-    )
+      <Grid
+        container
+        direction="row-reverse"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: "#faf0ca" }}
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              style={{ backgroundColor: "#faf0ca", fontWeight: "bold" }}
+            >
+              Ou...
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              style={{ color: "#ff0054", fontWeight: "bold" }}
+            >
+              Veja os nossos temas!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Link to="/temas" className="text-decorator-none">
+              <Box marginRight={1}></Box>
+              <Button className="botao-tema" variant="outlined">
+                Ver Temas
+              </Button>
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="../../../src/assets/images/imagem2.svg"
+            alt="logo"
+            width="550px"
+            height="300px"
+          />
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: "white" }}></Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
