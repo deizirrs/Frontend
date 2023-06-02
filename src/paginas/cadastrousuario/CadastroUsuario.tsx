02f5c2a0faa1f205c9377acc52c968b758dff601
302kt <kpf.freire@gmail.com>
2023-06-02 13:23:34 -0300

    Adicionando tela de cadastro Estilizada

--- src/paginas/cadastrousuario/CadastroUsuario.tsx
+++ src/paginas/cadastrousuario/CadastroUsuario.tsx
@@ -61,32 +61,33 @@ function CadastroUsuario() {
 
     }
     return (
-        <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid container direction="row" justifyContent="center" alignItems="center" className='cor-fundo'>
             <Grid item className="imgCadastro" xs={6}>
             </Grid>
 
-            <Grid item xs={6} alignItems="center">
-                <Box padding={10}>
+            <Grid item xs={6} alignItems="center" className="elementos">
+                <Box className="box-cadastrar">
                     <form onSubmit={onSubmit}>
-                        <Typography variant='h3' className="textoCadastro" gutterBottom color='textPrimary' component='h3' align='center'>Cadastre-se</Typography>
-                        <TextField value={user.nomeUsuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nomeUsuario' label='nome' variant="outlined" name='nomeUsuario' margin='normal' fullWidth />
-                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario (e-mail)' variant="outlined" name='usuario' margin='normal' fullWidth />
-                        <TextField value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='foto' label='foto (link foto)' variant="outlined" name='foto' margin='normal' fullWidth />
-                        <TextField value={user.tipoUsuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='tipoUsuario' label='tipo de usuario (fisica/juridica)' variant="outlined" name='tipoUsuario' margin='normal' fullWidth />
-                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha (min: 8 caracteres)' variant="outlined" name='senha' margin='normal' type='password' fullWidth />
-                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' variant="outlined" name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <Typography className="textoCadastro" color='textPrimary' align='center'>Cadastre-se</Typography>
+                        <TextField className='custom-textfield' value={user.nomeUsuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nomeUsuario' label='nome' variant="outlined" name='nomeUsuario' margin='dense'fullWidth/>
+                        <TextField className='custom-textfield' value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario (e-mail)' variant="outlined" name='usuario' margin='dense' fullWidth />
+                        <TextField className='custom-textfield' value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='foto' label='foto (link foto)' variant="outlined" name='foto' margin='dense'fullWidth />
+                        <TextField className='custom-textfield' value={user.tipoUsuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='tipoUsuario' label='tipo de usuario (fisica/juridica)' variant="outlined" name='tipoUsuario' margin='dense' fullWidth />
+                        <TextField className='custom-textfield' value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha (min: 8 caracteres)' variant="outlined" name='senha' margin='dense' type='password' fullWidth />
+                        <TextField className='custom-textfield' value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' variant="outlined" name='confirmarSenha' margin='dense' type='password' fullWidth />
 
                         <Box marginTop={2} textAlign="center">
 
-                            <Button type='submit' variant='contained' className='btnCadastrar'>
+                            <Button type='submit' variant='contained' className='cadastrar'>
                                 Cadastrar
                             </Button>
 
                             <Link to='/login'>
-                                <Button variant='contained' className='btnCancelar'>
+                                <Button variant='contained' className='cancelar' style={{ margin: '15px' }}>
                                     Cancelar
                                 </Button>
                             </Link>
+
                         </Box>
                     </form>
                 </Box>
