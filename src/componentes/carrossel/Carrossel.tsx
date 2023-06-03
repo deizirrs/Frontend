8eaa7a5712581f986c42307d15d7794f224afb91
Michelle Greghi <michelle.neves23@gmail.com>
2023-06-02 21:22:36 -0300

    Atualização para Deploy

--- src/componentes/carrossel/Carrossel.tsx
+++ src/componentes/carrossel/Carrossel.tsx
@@ -1,178 +1,178 @@
-import { Box, Button, Grid, Typography } from "@material-ui/core";
-import React from "react";
-import "./Carrossel.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper";
-import { useNavigate } from "react-router-dom";
+// import { Box, Button, Grid, Typography } from "@material-ui/core";
+// import React from "react";
+// import "./Carrossel.css";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import { Autoplay, Pagination, Navigation } from "swiper";
+// import { useNavigate } from "react-router-dom";
 
-function Carrossel() {
-  const navigate = useNavigate();
+// function Carrossel() {
+//   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/login");
-  };
+//   const handleLogin = () => {
+//     navigate("/login");
+//   };
 
-  const handleSignup = () => {
-    navigate("/cadastrar");
-  };
+//   const handleSignup = () => {
+//     navigate("/cadastrar");
+//   };
 
-  return (
-    <>
-    <Grid container spacing={2} style={{ backgroundColor: "#F4ECF7" }}>
-        <Grid item xs={12} md={6}>
-          <Box
-            style={{
-              backgroundColor: "#F4ECF7",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              padding: "10px",
-            }}
-          >
-            <Typography
-              variant="h4"
-              gutterBottom
-              color="textPrimary"
-              component="h6"
-              align="center"
-              style={{
-                color: "#3c096c",
-                fontWeight: "bold",
-                fontSize: "24px",
-              }}
-            >
-              Bem-vinda à nossa comunidade!
-            </Typography>
+//   return (
+//     <>
+//     <Grid container spacing={2} style={{ backgroundColor: "#F4ECF7" }}>
+//         <Grid item xs={12} md={6}>
+//           <Box
+//             style={{
+//               backgroundColor: "#F4ECF7",
+//               display: "flex",
+//               flexDirection: "column",
+//               justifyContent: "center",
+//               alignItems: "center",
+//               padding: "10px",
+//             }}
+//           >
+//             <Typography
+//               variant="h4"
+//               gutterBottom
+//               color="textPrimary"
+//               component="h6"
+//               align="center"
+//               style={{
+//                 color: "#3c096c",
+//                 fontWeight: "bold",
+//                 fontSize: "24px",
+//               }}
+//             >
+//               Bem-vinda à nossa comunidade!
+//             </Typography>
 
-            <Typography
-              variant="h6"
-              gutterBottom
-              color="textPrimary"
-              component="h6"
-              style={{
-                color: "#3c096c",
-                fontWeight: "bold",
-                marginTop: "20px",
-                textAlign: "justify",
-                fontSize: "20px",
-              }}
-            >
-              Uma rede social dedicada ao empoderamento feminino, ao apoio mútuo e
-              ao crescimento profissional. Aqui, você encontrará um espaço acolhedor
-              e inspirador, onde mulheres de todas as áreas podem se conectar,
-              aprender e se desenvolver. Nossa missão é fornecer um ambiente seguro e
-              inclusivo, onde você possa compartilhar suas conquistas, desafios e
-              aspirações. Estamos aqui para encorajá-la a alcançar seus sonhos,
-              independentemente do setor em que deseja atuar.
-            </Typography>
+//             <Typography
+//               variant="h6"
+//               gutterBottom
+//               color="textPrimary"
+//               component="h6"
+//               style={{
+//                 color: "#3c096c",
+//                 fontWeight: "bold",
+//                 marginTop: "20px",
+//                 textAlign: "justify",
+//                 fontSize: "20px",
+//               }}
+//             >
+//               Uma rede social dedicada ao empoderamento feminino, ao apoio mútuo e
+//               ao crescimento profissional. Aqui, você encontrará um espaço acolhedor
+//               e inspirador, onde mulheres de todas as áreas podem se conectar,
+//               aprender e se desenvolver. Nossa missão é fornecer um ambiente seguro e
+//               inclusivo, onde você possa compartilhar suas conquistas, desafios e
+//               aspirações. Estamos aqui para encorajá-la a alcançar seus sonhos,
+//               independentemente do setor em que deseja atuar.
+//             </Typography>
 
-            <Box display="flex" justifyContent="center" marginTop="20px">
-              <Button
-                className="botao"
-                variant="contained"
-                color="primary"
-                onClick={handleLogin}
-                style={{ marginRight: "10px" }}
-              >
-                Login
-              </Button>
-              <Button
-                className="botao"
-                variant="contained"
-                color="secondary"
-                onClick={handleSignup}
-              >
-                Cadastre-se
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
+//             <Box display="flex" justifyContent="center" marginTop="20px">
+//               <Button
+//                 className="botao"
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={handleLogin}
+//                 style={{ marginRight: "10px" }}
+//               >
+//                 Login
+//               </Button>
+//               <Button
+//                 className="botao"
+//                 variant="contained"
+//                 color="secondary"
+//                 onClick={handleSignup}
+//               >
+//                 Cadastre-se
+//               </Button>
+//             </Box>
+//           </Box>
+//         </Grid>
         
       
           
       
-      {/* <Grid container spacing={2} style={{
-              backgroundColor: "#F4ECF7"}} >
-        <Grid item xs={12} md={6} >
-          <Box
-            style={{
-              backgroundColor: "#F4ECF7",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              padding: "50px",
-            }}
-          >
-            <Typography
-              variant="h4"
-              gutterBottom
-              color="textPrimary"
-              component="h6"
-              align="center"
-              style={{ color: "#3c096c", fontWeight: "bold", fontSize: "24px" }}
-            >
-              Bem-vinda à nossa comunidade!
-            </Typography>
+//       {/* <Grid container spacing={2} style={{
+//               backgroundColor: "#F4ECF7"}} >
+//         <Grid item xs={12} md={6} >
+//           <Box
+//             style={{
+//               backgroundColor: "#F4ECF7",
+//               display: "flex",
+//               flexDirection: "column",
+//               justifyContent: "center",
+//               alignItems: "center",
+//               padding: "50px",
+//             }}
+//           >
+//             <Typography
+//               variant="h4"
+//               gutterBottom
+//               color="textPrimary"
+//               component="h6"
+//               align="center"
+//               style={{ color: "#3c096c", fontWeight: "bold", fontSize: "24px" }}
+//             >
+//               Bem-vinda à nossa comunidade!
+//             </Typography>
 
-            <Typography
-              variant="h6"
-              gutterBottom
-              color="textPrimary"
-              component="h6"
-              style={{
-                color: "#3c096c",
-                fontWeight: "bold",
-                marginTop: "20px",
-                textAlign: "justify",
-                fontSize: "20px"
-              }}
-            >
-              Uma rede social dedicada ao empoderamento feminino, ao apoio mútuo e
-              ao crescimento profissional. Aqui, você encontrará um espaço acolhedor
-              e inspirador, onde mulheres de todas as áreas podem se conectar,
-              aprender e se desenvolver. Nossa missão é fornecer um ambiente seguro e
-              inclusivo, onde você possa compartilhar suas conquistas, desafios e
-              aspirações. Estamos aqui para encorajá-la a alcançar seus sonhos,
-              independentemente do setor em que deseja atuar.
-            </Typography>
-          </Box>
-        </Grid> */}
+//             <Typography
+//               variant="h6"
+//               gutterBottom
+//               color="textPrimary"
+//               component="h6"
+//               style={{
+//                 color: "#3c096c",
+//                 fontWeight: "bold",
+//                 marginTop: "20px",
+//                 textAlign: "justify",
+//                 fontSize: "20px"
+//               }}
+//             >
+//               Uma rede social dedicada ao empoderamento feminino, ao apoio mútuo e
+//               ao crescimento profissional. Aqui, você encontrará um espaço acolhedor
+//               e inspirador, onde mulheres de todas as áreas podem se conectar,
+//               aprender e se desenvolver. Nossa missão é fornecer um ambiente seguro e
+//               inclusivo, onde você possa compartilhar suas conquistas, desafios e
+//               aspirações. Estamos aqui para encorajá-la a alcançar seus sonhos,
+//               independentemente do setor em que deseja atuar.
+//             </Typography>
+//           </Box>
+//         </Grid> */}
 
-        <Grid item xs={12} md={6}>
-          <Box style={{ backgroundColor: "#F4ECF7" }}>
-            <Swiper
-              slidesPerView={1}
-              spaceBetween={10}
-              loop={true}
-              pagination={{
-                clickable: true,
-              }}
-              autoplay={{
-                delay: 5000,
-                disableOnInteraction: false,
-              }}
-              navigation={true}
-              modules={[Autoplay, Pagination, Navigation]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <img src="../../../src/assets/images/mulheres.png" alt="Imagem" />
-              </SwiperSlide>
+//         <Grid item xs={12} md={6}>
+//           <Box style={{ backgroundColor: "#F4ECF7" }}>
+//             <Swiper
+//               slidesPerView={1}
+//               spaceBetween={10}
+//               loop={true}
+//               pagination={{
+//                 clickable: true,
+//               }}
+//               autoplay={{
+//                 delay: 5000,
+//                 disableOnInteraction: false,
+//               }}
+//               navigation={true}
+//               modules={[Autoplay, Pagination, Navigation]}
+//               className="mySwiper"
+//             >
+//               <SwiperSlide>
+//                 <img src="../../../src/assets/images/mulheres.png" alt="Imagem" />
+//               </SwiperSlide>
 
-              <SwiperSlide>
-                <img src="../../../src/assets/images/home.jpg" alt="Imagem" />
-              </SwiperSlide>
-            </Swiper>
-          </Box>
-        </Grid>
-      </Grid>
-    </>
-  );
-}
+//               <SwiperSlide>
+//                 <img src="../../../src/assets/images/home.jpg" alt="Imagem" />
+//               </SwiperSlide>
+//             </Swiper>
+//           </Box>
+//         </Grid>
+//       </Grid>
+//     </>
+//   );
+// }
 
-export default Carrossel;
+// export default Carrossel;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,8 +1,6 @@
-
-
 import { Box, Grid, Typography, Tab, Tabs } from "@mui/material";
 import React, { useState } from "react";
-import Carrossel from "../../componentes/carrossel/Carrossel";
+// import Carrossel from "../../componentes/carrossel/Carrossel";
 import Sobre from "../../paginas/sobre/Sobre";
 import TabHome from "./tabhome/TabHome";
 import Missao from "../missao/Missao";
@@ -11,7 +9,7 @@ import Missao from "../missao/Missao";
 function Home() {
   const [tabIndex, setTabIndex] = useState(0);
 
-  const [value, setValue] = useState('1')
+  const [value, setValue] = useState("1");
   function handleChange(event: React.ChangeEvent<{}>, newValue: string) {
     setValue(newValue);
   }
@@ -25,37 +23,37 @@ function Home() {
         // alignItems="center"
         style={{ backgroundColor: "#c77dff" }}
       >
-        <Grid >
+        <Grid>
           <Box>
             <Box>
-              <Grid >
+              <Grid>
                 <img
                   className="imagemLogo"
                   src="../../../src/assets/images/ufa_logo.roxo.png"
                   alt=""
-                  style={{ width: '94px', height: '80px' }} // Ajuste o tamanho conforme desejado
+                  style={{ width: "94px", height: "80px" }} // Ajuste o tamanho conforme desejado
                 />
               </Grid>
             </Box>
           </Box>
         </Grid>
-
-
       </Grid>
-
 
       <Grid container style={{ marginTop: "2px" }}>
         <Grid item xs={12}>
-          <Carrossel />
+          {/* <Carrossel /> */}
+           <img
+            className="imagemHome"
+            src="../../../src/assets/images/home.jpg"
+            alt="Imagem"
+            style={{ width: "1350px", height: "800px" }}
+          /> 
         </Grid>
       </Grid>
 
       <Box sx={{ width: "100%" }}>
-
         <TabHome />
       </Box>
-      )
-
     </>
   );
 }
